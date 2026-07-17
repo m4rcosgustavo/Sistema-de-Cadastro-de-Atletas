8311530aed161ce02dada820878f7c0ce825565a
m4rcosgustavo <marcos.gustavo@escolar.ifrn.edu.br>
2026-07-17 00:44:04 -0300

    Cria modelo User

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+    senha = db.Column(db.String(255), nullable=False)
