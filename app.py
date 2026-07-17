c1297bb019041bd83d93a74d97db5ec2c0ea0458
m4rcosgustavo <marcos.gustavo@escolar.ifrn.edu.br>
2026-07-17 00:42:46 -0300

    Configura SQLAlchemy no projeto

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from models import db, User, Atleta, Esporte
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.secret_key = "chave_secreta_para_sessao"
-
+app.secret_key = 'chave_secreta_para_sessao'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///banco.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
 
 @app.route('/')
 def index():
@@ -284,4 +281,5 @@ def delete_atleta(id):
     return redirect(url_for('atletas'))
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
