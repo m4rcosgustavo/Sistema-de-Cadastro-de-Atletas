c2c9b3e70abef8ed8e4e18ba1dd5082f26582175
m4rcosgustavo <marcos.gustavo@escolar.ifrn.edu.br>
2026-07-17 00:44:24 -0300

    Cria modelo Atleta

--- models.py
+++ models.py
@@ -8,3 +8,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=True, nullable=False)
     senha = db.Column(db.String(255), nullable=False)
+
+class Atleta(db.Model):
+    __tablename__ = "atletas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    idade = db.Column(db.Integer, nullable=False)
+    esporte = db.Column(db.String(100), nullable=False)
